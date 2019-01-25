4e110f3711b2ff2916b1f307a343722c8728d394
Peter K <peterkohler95@gmail.com>
2019-01-24 19:26:50 -0600

    added redux store

--- rrtutorialproj/src/components/Postform.js
+++ rrtutorialproj/src/components/Postform.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { createPost } from '../actions/postActions'
 
 class Postform extends Component {
     constructor(props) {
@@ -22,15 +25,8 @@ class Postform extends Component {
             title: this.state.title,
             body: this.state.body
         }
-        fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(post)
-        })
-            .then(res => res.json())
-            .then(data => console.log(data))
+
+        this.props.createPost(post)
     }
 
     render() {
@@ -55,5 +51,8 @@ class Postform extends Component {
     }
 }
 
+Postform.propTypes = {
+    createPost: PropTypes.func.isRequired
+}
 
-export default Postform
+export default connect(null, { createPost })(Postform)
